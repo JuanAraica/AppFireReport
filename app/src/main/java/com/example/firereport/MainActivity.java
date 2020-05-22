774d5f1bb709150a0041a88dedfdde720794680e
aray <juanaraica149@gmail.com>
2020-05-21 20:29:20 -0600

    actualizacion de androis studio

--- app/src/main/java/com/example/firereport/MainActivity.java
+++ app/src/main/java/com/example/firereport/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.firereport;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
